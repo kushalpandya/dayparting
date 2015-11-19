9bb0a18a1604ec51f4c9f0bfa097238ef2e2ca17
Kushal Pandya <kushalpandya@outlook.com>
2015-11-19 16:02:58 +0530

    First alpha of Dayparting.

--- dayparting.js
+++ dayparting.js
@@ -16,15 +16,17 @@
         daypart;
 
     daypart = self.daypart = function(config) {
-        var thisDaypart = this,
-            TODAY = new Date(),
-            daypartConfig,
-            isValidDateObj;
+        var TODAY = new Date(),
+            thisDaypart = {},
+            daypartConfig = {};
+
+        var fnIsValidDateObj,
+            fnGetDaypartSlot;
 
         /**
          * Date object validity check < http://stackoverflow.com/a/1353711/414749 >.
          */
-        isValidDateObj = function() {
+        fnIsValidDateObj = function(dateObj) {
             if (Object.prototype.toString.call(dateObj) === '[object Date]')
             {
                 if (!isNaN(dateObj.getTime()))
@@ -37,15 +39,55 @@
         };
 
         /**
-         * Initialize Locale strings.
+         * Resolve appropriate Daypart from the time present in given date object.
+         *
+         * Core Logic behind Dayparting is in this method, it follows the slots given in https://en.wikipedia.org/wiki/Dayparting.
+         * You can change these slots if you're targetting a particular region of the world which has different slots.
          */
-        thisDaypart.initlocale = function(userLocale) {
-            var thisDaypart = this,
-                defaultLocale;
+        fnGetDaypartSlot = function(dateObj) {
+            var earlyMorning    =   { start: 600,   end: 900 },
+                lateMorning     =   { start: 900,   end: 1200 },
+                afternoon       =   { start: 1200,  end: 1700 },
+                earlyFringe     =   { start: 1700,  end: 2000 },
+                lateFringe      =   { start: 2000,  end: 2300 },
+                lateNight       =   { start: 2300,  end: 200 },
+                overnight       =   { start: 200,   end: 600 };
 
-            if (typeof userLocale === 'object')
-                thisDaypart.locale = userLocale;
+            var hh = dateObj.getHours(),
+                mm = dateObj.getMinutes(),
+                fnIsInSlot,
+                slotName,
+                hhmm;
+
+            fnIsInSlot = function(slotRange, timeval) {
+                return (timeval >= slotRange.start && timeval <= slotRange.end);
+            };
+
+            hhmm = parseInt(hh + '' + (mm < 10 ? ('0' + mm) : mm)); // Convert hh:mm in 1000s format.
+
+            if (fnIsInSlot(earlyMorning, hhmm))
+                slotName = 'earlyMorning';
+            else if (fnIsInSlot(lateMorning, hhmm))
+                slotName = 'lateMorning';
+            else if (fnIsInSlot(afternoon, hhmm))
+                slotName = 'afternoon';
+            else if (fnIsInSlot(earlyFringe, hhmm))
+                slotName = 'earlyFringe';
+            else if (fnIsInSlot(lateFringe, hhmm))
+                slotName = 'lateFringe';
+            else if (fnIsInSlot(lateNight, hhmm))
+                slotName = 'lateNight';
+            else if (fnIsInSlot(overnight, hhmm))
+                slotName = 'overnight';
+
+            return slotName;
         };
+
+        if (typeof config === 'object')
+        {
+            daypartConfig.locale = config.locale || 'en-US'; // Defaults to en-US locale.
+            daypartConfig.localeJSON = (typeof config.localeJSON === 'object') ? config.localeJSON : {}; // Make sure we don't set undefined instead of an object;
+        }
 
         /**
          * Resolve instance conflict in case module is loaded more than once.
@@ -62,9 +104,13 @@
          * @param dateObj a valid Date object.
          */
         thisDaypart.for = function(dateObj) {
-            if (isValidDateObj(dateObj))
+            var datepartSlot,
+                currLocaleDaypart = daypartConfig.localeJSON[daypartConfig.locale];
+
+            if (fnIsValidDateObj(dateObj))
             {
-                console.log('This is chaos', dateObj);
+                datepartSlot = fnGetDaypartSlot(dateObj);
+                return currLocaleDaypart[datepartSlot];
             }
             else
                 throw new Error("Date is invalid.");
