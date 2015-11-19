058df5e0ae5286e3c2c225ce3f21f3b5ef1dc5d7
Kushal Pandya <kushalpandya@outlook.com>
2015-11-19 11:53:37 +0530

    Mocha test-suite added.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/**
+ * dayparting  tests v1.0.0
+ *
+ * Author: Kushal Pandya <kushalspandya@gmail.com> (https://doublslash.com)
+ * Date: 18 November, 2015
+ *
+ * Dayparting Test Script for Mocha.
+ */
